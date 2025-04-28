a5af698812fae1efb1f37674121ea04bc612f8a4
archit20122001 <archit20122001@gmail.com>
2025-04-28 18:31:48 +0530

    Static_functions -- Class name standardised

--- Static_functions.cpp
+++ Static_functions.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-class complex
+class Complex
 {
     int a;
     int b;
@@ -11,11 +11,11 @@ class complex
             a = v1;
             b = v2;
         }
-        // void setDataBySum(complex o1, complex o2)
-        // {
-        //     a = o1.a + o2.a;
-        //     b = o1.b + o2.b;
-        // }
+        void setDataBySum(Complex o1, Complex o2)
+        {
+            a = o1.a + o2.a;
+            b = o1.b + o2.b;
+        }
         void printNumber()
         {
             cout<<"Your complex number is "<<a<<" + "<<b<<"i"<<endl;
@@ -23,7 +23,7 @@ class complex
 };
 int main()
 {
-    complex c1, c2, c3;
+    Complex c1, c2, c3;
     c1.setData(1, 2);
     c1.printNumber();
     c2.setData(3, 4);
