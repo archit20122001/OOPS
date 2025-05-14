e23f30d74fad657110f5323bb4a329c09aa502da
archit20122001 <archit20122001@gmail.com>
2025-05-14 19:50:35 +0530

    This pointer and its uses in class

--- This-pointer.cpp
+++ This-pointer.cpp
@@ -5,21 +5,24 @@ class A
 {
     int a;
     public:
-        void setData(int a)
+        // void setData(int a)
+        A & setData(int a)
         {
             this->a = a;
+            return *this;
         }
 
-        void getData()
-        {
-            cout<<"The value of a is: "<<a<<endl;
+        void getData(){
+            cout<<"The value of a is "<<a<<endl;
         }
 };
+
 int main()
 {
     A a;
-    a.setData(4);
-    a.getData();
+    a.setData(4).getData();
+    // a.setData(4);
+    // a.getData();
     return 0;
 }
 
